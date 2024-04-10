fc6c617f631db3818a00a9e49ae5619cce241b98
Techie Sam <119287876+TechieSamDev@users.noreply.github.com>
2024-04-10 06:01:21 +0100

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,6 @@ import { childVariant, containerVariant } from "./HeaderAnimationStyles";
 const items = [
   <h5 style={{ fontSize: 15 }}>Hey, I'm</h5>,
   <h1>Samuel Abolade.</h1>,
-  <h6>Techie Sam</h6>,
   <h6>Software Engineer</h6>,
 ];
 
