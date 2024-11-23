92e70cff7a704dd1dcc2f1936f83117bf9f64b4c
Techie Sam <blessingabolade15@gmail.com>
2024-11-23 14:27:17 +0100

    updated aout cards -changing complted projects section

--- src/components/about/AboutCards.jsx
+++ src/components/about/AboutCards.jsx
@@ -1,4 +1,4 @@
-import { HiBriefcase, HiClock } from 'react-icons/hi2';
+import {  HiClock, HiCommandLine } from "react-icons/hi2";
 
 const currentDate = new Date();
 
@@ -6,22 +6,22 @@ const AboutCards = () => {
   return (
     <div>
       <ul className="grid gap-5">
-        <li className="col-span-6  bg-secondary hover:bg-secondary/20 border border-slate-600 p-5 rounded-lg text-center transition-all duration-150">
+        <li className="col-span-6  bg-secondary hover:bg-secondary/20 border border-slate-600 p-5 space-y-1 rounded-lg text-center transition-all duration-150">
           <h3 className="py-3">
             <HiClock className="text-3xl m-auto" />
           </h3>
 
-          <h6>Experience</h6>
+          <h6 className="font-bold">Experience</h6>
           <small className="mt-5 p-3">2+ years</small>
         </li>
 
-        <li className="col-span-6 bg-secondary hover:bg-secondary/20 border border-slate-600 p-5 rounded-lg text-center transition-all duration-150">
+        <li className="col-span-6 bg-secondary hover:bg-secondary/20 border border-slate-600 p-5 space-y-1 rounded-lg text-center transition-all duration-150">
           <h3 className="py-3">
-            <HiBriefcase className="text-3xl m-auto" />
+            <HiCommandLine className="text-3xl m-auto" />
           </h3>
 
-          <h6>Projects</h6>
-          <small>10+ Completed</small>
+          <h6 className="font-bold">Development Focus</h6>
+          <small>Frontend & Backend</small>
         </li>
       </ul>
     </div>
--- src/components/about/HeadShot.jsx
+++ src/components/about/HeadShot.jsx
@@ -1,9 +1,9 @@
 const HeadShot = () => {
   return (
-    <div className="relative flex items-center justify-center filter-none ">
-      <div className="w-max rounded-full bg-secondary border border-slate-800">
+    <div className="relative  flex items-center justify-center filter-none ">
+      <div className="w-max rounded-full bg-secondary/30 shadow border border-slate-800">
         <img
-          className="mix-blend-overlay hover:mix-blend-normal filter-none m-auto"
+          className="mix-blend-overlay hover:mix-blend-normal transition-all duration-300 filter-none m-auto"
           src="/assets/avatar.png"
           width="250"
           height="250"
