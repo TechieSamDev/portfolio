17419181ddd4134e4769ab114737d20f579d80fb
Techie Sam <blessingabolade15@gmail.com>
2024-11-28 04:03:50 +0100

    removed blend stuff from head shot

--- src/components/about/HeadShot.jsx
+++ src/components/about/HeadShot.jsx
@@ -3,7 +3,7 @@ const HeadShot = () => {
     <div className="relative  flex items-center justify-center filter-none ">
       <div className="w-max rounded-full bg-secondary/30 shadow border border-slate-800">
         <img
-          className="mix-blend-overlay hover:mix-blend-normal transition-all duration-300 filter-none m-auto"
+          className="m-auto"
           src="/assets/avatar.png"
           width="250"
           height="250"
