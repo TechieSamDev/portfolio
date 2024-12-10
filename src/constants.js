eaca5f9d16a0d30055cc29307b92d1cbd7267be1
Techie Sam <blessingabolade15@gmail.com>
2024-12-10 05:17:39 +0100

    updated projects

--- src/constants.js
+++ src/constants.js
@@ -58,28 +58,21 @@ export const TECHNOLOGIES = [
 
 export const PROJECTS = [
   {
+    name: "Baby FROG - $BFOWG",
+    image: "/assets/bfwog-cover.png",
+    // gitHub: "https://github.com/techie-sam/quizlet",
+    liveDemo: "https://bfwog.vercel.app",
+    description:
+      "A clean and simple page for $BabyFwog, which gives visitors everything they need without making them hunt for it. From token buying guides to smart contract details and the project roadmap, all the key info is easy to find in a friendly and modern design.",
+  },
+  {
     name: "$BSC - Building Strong Community",
     image: "/assets/bsc-cover.png",
     liveDemo: "https://bsc.vercel.app",
     description:
       "A Web3 project website made to help users easily find everything they need. It includes the project's smart contract address, contact links, a decentralized exchange (DEX), and a simple guide on how to buy the token.",
   },
-  {
-    name: "Heal Me  Non-profit Organization",
-    image: "/assets/heal-me-cover.png",
-    // gitHub: "https://github.com/techie-sam/quizlet",
-    liveDemo: "https://heal-me-frontend.vercel.app",
-    description:
-      "A non-profit organization that helps orphaned children, hospital patients, new mothers, and individuals with disabilities. I focused on designing the parts of the website people interact with, and suggested ways to improve the backend.",
-  },
-  // {
-  //   name: "V Bank",
-  //   liveDemo: "https://vbank.vercel.app/",
-  //   image: "/assets/V-bank.png",
-  //   gitHub: "https://github.com/techie-sam/v-bank",
-  //   description:
-  //     "A functional banking solution which offers a range of basic features. It allows users to simulate transactions, manage accounts, and view balances in a secure and reliable environment.",
-  // },
+
   {
     name: "$FLOOF",
     liveDemo: "https://floofcto.vercel.app",
@@ -87,5 +80,13 @@ export const PROJECTS = [
     // gitHub: "https://github.com/techie-sam/cbt_react_app",
     description:
       "A Web3 project website redesigned to improve its look, usability, and overall user experience. I created a modern, user-friendly design that aligns with the project's vision and enhances user engagement",
+  },
+  {
+    name: "Heal Me  Non-profit Organization", 
+    image: "/assets/heal-me-cover.png",
+    // gitHub: "https://github.com/techie-sam/quizlet",
+    // liveDemo: "https://heal-me-frontend.vercel.app",
+    description:
+      "A non-profit organization that helps orphaned children, hospital patients, new mothers, and individuals with disabilities. I focused on designing the parts of the website people interact with, and suggested ways to improve the backend.",
   },
 ];
