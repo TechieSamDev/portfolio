2e529a9214b90bdd8d46820882ba2c547185634b
Techie Sam <blessingabolade15@gmail.com>
2025-06-11 21:37:02 +0100

    added animation for abous section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,20 +1,35 @@
+import { motion } from "motion/react";
 import HorizontalRule from "../utils/HorizontalRule";
 import LinkButton from "../utils/LinkButton";
 import HeadShot from "./HeadShot";
 
+const variants = {
+  hidden: { opacity: 0, y: 50 },
+  visible: { opacity: 1, y: 0, transition: { delay: 0.2, duration: 0.7 } },
+  viewport: { once: true },
+  transition: {
+    // duration: 1,
+    ease: "easeOut",
+  },
+};
 function About() {
   return (
     <section id="about" className="max-w-5xl md:px-10 px-5">
-      <div className=" space-y-10 py-10">
-        <div className="flex items-center">
+      <motion.div
+        whileInView="visible"
+        initial="hidden"
+        variants={variants}
+        viewport={{ once: true }}
+        className=" space-y-10 py-10"
+      >
+        <div variants={variants} className="flex items-center">
           <h5 className="text-2xl whitespace-nowrap pr-2 px text-slate-200">
             <span className="text-sm text-accent px-2">01. </span> About me
           </h5>
           <HorizontalRule />
         </div>
-
-        <div className="md:flex space-y-10 border">
-          <div className="space-y-3 border">
+        <div className="md:flex md:ml-10  space-y-10">
+          <div className="space-y-3">
             <div className="">
               {/* <div className="md:flex space-y-5 gap-5 md:px-12"></div> */}
               <p className="tracking-wide text-slate-400 px-2">
@@ -27,7 +42,7 @@ function About() {
                 Nesciunt!
               </p>
             </div>
-            <div className="px-5 ">
+            <div variants={variants} className="px-5 ">
               <ul className="grid grid-cols-2 p-2 md:max-w-80 relative text-slate-400 gap-2 justify-evenly">
                 <li>JavaScript</li>
                 <li>TypeScript</li>
@@ -50,7 +65,7 @@ function About() {
             Get in Touch
           </LinkButton>
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 }
--- src/components/about/HeadShot.jsx
+++ src/components/about/HeadShot.jsx
@@ -4,9 +4,9 @@ const HeadShot = () => {
       <img
         className="m-auto mix-blend-multiply  border border-slate-600 shadow-lg  transition-all duration-300"
         src="/assets/avatar.png"
-        width="200"
-        height="200"
-        alt="head-shot"
+        width="250"
+        height="250"
+        alt="techiesam-pfp"
       />
 
       <div className="absolute  left-3 -bottom-3 group-hover:-translate-x-1 group-hover:-translate-y-1 transition-all size-full border border-accent rounded-tr-md rounded-bl-md -z-10"></div>
