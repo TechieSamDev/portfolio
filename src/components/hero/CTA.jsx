d2fb042fa5fe45ed95a0405a2afde651d9e7eb51
Techie Sam <blessingabolade15@gmail.com>
2025-08-24 01:00:28 +0100

    chore: updated resume

--- src/components/hero/CTA.jsx
+++ src/components/hero/CTA.jsx
@@ -5,8 +5,8 @@ const CTA = () => {
     <div className="flex gap-5">
       <LinkButton
         variant="accent"
-        href="/assets/001Techie_Sam-Resume.pdf"
-        download="001Techie_Sam-Resume.pdf"
+        href="/assets/001Abolade-Samuel(Techie-Sam)-Resume.pdf"
+        download="001Abolade-Samuel(Techie-Sam)-Resume.pdf"
         className="border border-accent text-accent py-3 px-4 rounded-lg transition duration outline-none flex items-center  focus:bg-accent/50 focus:text-slate-100"
       >
         Get Resume
